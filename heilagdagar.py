9464008fa55b552c85175b0a1b36447e09b313f2
Johannes Groedem <johs@grdm.no>
2022-02-12 11:32:03 +0100

    Make it actually work, outputting what looks like a sane calendar.

--- heilagdagar.py
+++ heilagdagar.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
-import sys
 import time
 import re
-from pprint import pprint
 from datetime import timedelta, date, datetime
 
 from dateutil.easter import easter
@@ -13,23 +11,23 @@ from ics import Calendar, Event
 def generate_norwegian_holidays(year):
     easter_sunday = easter(year)
 
-    return {
+    return [
         # Fixed holidays
-        "Nyttårsdag": date(year, 1, 1),
-        "Førstedag jul": date(year, 12, 25),
-        "Annandag jul": date(year, 12, 26),
-        "Internasjonal arbeidardag": date(year, 5, 1),
-        "Grunnlovsdagen": date(year, 5, 17),
+        (date(year, 1, 1), "Nyttårsdag"),
+        (date(year, 12, 25), "Førstedag jul"),
+        (date(year, 12, 26), "Annandag jul"),
+        (date(year, 5, 1), "Internasjonal arbeidardag"),
+        (date(year, 5, 17), "Grunnlovsdagen"),
         # Easter, pentecost, etc.
-        "Skjærtorsdag": easter_sunday - timedelta(days=3),
-        "Langfredag": easter_sunday - timedelta(days=2),
-        "Påskeaftan": easter_sunday - timedelta(days=1),
-        "Påskedagen": easter_sunday,
-        "Annandag påske": easter_sunday + timedelta(days=1),
-        "Kristi himmelfartsdag": easter_sunday + timedelta(days=39),
-        "Førstedag pinse": easter_sunday + timedelta(days=49),
-        "Annandag pinse": easter_sunday + timedelta(days=50),
-    }
+        (easter_sunday - timedelta(days=3), "Skjærtorsdag"),
+        (easter_sunday - timedelta(days=2), "Langfredag"),
+        (easter_sunday - timedelta(days=1), "Påskeaftan"),
+        (easter_sunday, "Påskedagen"),
+        (easter_sunday + timedelta(days=1), "Annandag påske"),
+        (easter_sunday + timedelta(days=39), "Kristi himmelfartsdag"),
+        (easter_sunday + timedelta(days=49), "Førstedag pinse"),
+        (easter_sunday + timedelta(days=50), "Annandag pinse"),
+    ]
 
 
 def make_uid(name: str, year):
@@ -37,29 +35,32 @@ def make_uid(name: str, year):
     return f"no-{clean}-{year}@example.com"
 
 
+def make_holiday_event(created, name, dt):
+    e = Event()
+    e.uid = make_uid(name, dt.year)
+    e.name = name
+    e.begin = dt
+    e.make_all_day()
+    e.duration = timedelta(days=1)
+    e.created = created
+    return e
+
+
 def make_ical_file(holidays):
-    cal = Calendar()
     created = datetime.now().astimezone()
-
-    sorted_holidays = sorted(holidays.items(), key=lambda x: x[1])
-    for name, dt in sorted_holidays:
-        e = Event()
-        e.uid = make_uid(name, dt.year)
-        e.name = name
-        e.begin = dt
-        e.make_all_day()
-        # e.duration = timedelta(days=1)
-        e.created = created
-        cal.events.add(e)
-
-    return cal
+    return Calendar(
+        events=[make_holiday_event(created, name, dt) for dt, name in holidays]
+    )
 
 
 def main():
     current_year = time.localtime().tm_year
-    year = int(sys.argv[1]) if len(sys.argv) > 1 else current_year
-    holidays = generate_norwegian_holidays(year)
-    print(make_ical_file(holidays, 2022, 2024))
+    holidays = [
+        holiday
+        for year in range(current_year - 1, current_year + 6)
+        for holiday in generate_norwegian_holidays(year)
+    ]
+    print(make_ical_file(holidays))
 
 
 if __name__ == "__main__":
